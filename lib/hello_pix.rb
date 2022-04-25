a7769bfaa72ed99e4735e86250e52c387795d0ad
Henrique Silva <henrique@henriquesilva.dev>
2022-04-25 15:08:58 -0300

    Initiated project

---
+++ lib/hello_pix.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hello_pix/version"
+
+module HelloPix
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hello_pix/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HelloPix
+  VERSION = "0.1.0"
+end
